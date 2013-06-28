2ddec6f620e97d31c8db21db353e3f54fc717435
luciamedina <luciamedinad@hotmail.com>
2013-06-27 20:13:35 -0500

    HOLA

---
+++ Carrera_pacman/Carreras_pacman/src/carreras_pacman/Clon.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carreras_pacman;
+
+/**
+ *
+ * @author JuanJesús
+ */
+public class Clon {
+    
+}
